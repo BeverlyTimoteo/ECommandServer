0d828bef6e1c4fb9c391e52d127bd32bddc2d336
Beverly Timóteo <beverlytimoteo@yahoo.com.br>
2020-06-20 08:13:29 -0300

    Tratamento de imagem

--- src/main/java/br/com/ecommandserver/entity/CategoriaEntity.java
+++ src/main/java/br/com/ecommandserver/entity/CategoriaEntity.java
@@ -15,7 +15,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity()
 @Table(name = "produtocategoria")
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class CategoriaEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -28,6 +28,9 @@ public class CategoriaEntity implements Serializable {
 	@NotNull(message = "Descrição obrigatória")
 	@Column(name = "descricao", length = 30, nullable = false)
 	private String descricao;
+
+	@Column(columnDefinition="TEXT")
+	private String imagem;
 
 	public Long getId() {
 		return id;
@@ -43,6 +46,51 @@ public class CategoriaEntity implements Serializable {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((imagem == null) ? 0 : imagem.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CategoriaEntity other = (CategoriaEntity) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (imagem == null) {
+			if (other.imagem != null)
+				return false;
+		} else if (!imagem.equals(other.imagem))
+			return false;
+		return true;
 	}
 
 }
